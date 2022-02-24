30ca6bc2799a45eb506bbdbae262b9f174a3a9cf
Lucas Roberto de Oliveira Barcelos <lucasolbarcelos@gmail.com>
2022-02-24 10:59:53 -0300

    [@LucassRoberto Task: Criação Spring initializr]

---
+++ app.farmacia/src/main/java/org/generation/app/farmacia/FarmaciaAppApplication.java
@@ -0,0 +1,13 @@
+package org.generation.app.farmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaAppApplication.class, args);
+	}
+
+}
---
+++ app.farmacia/src/test/java/org/generation/app/farmacia/FarmaciaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.app.farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
