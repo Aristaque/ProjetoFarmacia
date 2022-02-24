64330197634b9972d53c91bfdb58710e51eb5872
unknown <jeferson.aristaque@gmail.com>
2022-02-24 14:42:01 -0300

    @Aristaque [Task: Criando PostMapping]

--- app.farmacia/src/main/java/org/generation/app/farmacia/repository/CategoriaRepository.java
+++ app.farmacia/src/main/java/org/generation/app/farmacia/repository/CategoriaRepository.java
@@ -9,5 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
-	public List<Categoria> findAllByTipoContainingIgnoreCase(String tipo);
+	public List<Categoria> findAllBySetorContainingIgnoreCase(String setor);
 }
